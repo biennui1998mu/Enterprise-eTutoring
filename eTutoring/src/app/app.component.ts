c200c5ad2a31cfdb6a0e4c9c1790dfd52bb94d4a
Nguyen Son <valueislife@gmail.com>
2020-04-27 23:23:40 +0700

    update

---
+++ eTutoring/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+// import * as am4core from "@amcharts/amcharts4/core";
+// import * as am4charts from "@amcharts/amcharts4/charts";
+// import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'eTutoring';
+
+  renderChart() {
+    // const chart = am4core.create('chartdiv', am4charts.XYChart);
+  }
+}
