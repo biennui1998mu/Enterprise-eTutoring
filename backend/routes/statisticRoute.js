ba1f5c547c946710947b6ad7205386040acc9962
Nguyen Son <valueislife@gmail.com>
2020-05-02 22:38:16 +0700

    statistic for tutor dashboard

---
+++ backend/routes/statisticRoute.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
+
+const User = require('../database/models/user');
+const Classroom = require('../database/models/classroom');
+
+router.post('/tutor', checkAuth, async (req,res)=> {
+    const userId = req.userData._id;
+
+    const checkTutor = await User.findOne({
+        _id: userId
+    }).exec();
+
+    if(!checkTutor){
+        return res.json({
+            message: 'calling 911, you are a thief !!!'
+        })
+    }
+
+    if(checkTutor.level !== 2){
+        return res.json({
+            message: 'calling 911, there\'s no tutor like you, you are a thief !!!'
+        })
+    }
+
+    const classroom = await Classroom.find({
+        tutor: checkTutor
+    }).exec();
+
+    const message = await Classroom.find({
+        classroom: classroom
+    }).exec();
+
+    if(!message){
+        return res.json({
+            message: 'classroom dont have any message',
+            data: []
+        })
+    }
+
+    const file = await Classroom.find({
+        classroom: classroom
+    }).exec();
+
+    if(!file){
+        return res.json({
+            message: 'classroom dont have any file',
+            data: []
+        })
+    }
+
+    return res.json({
+        message: 'statistic for tutor dashboard',
+        data: {classroom, message, file}
+    })
+
+})
+
+
+module.exports = router;
+
+
+
+
+
