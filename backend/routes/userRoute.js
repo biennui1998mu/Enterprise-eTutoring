ca595cc489e3d82481075d42ba496b250677e945
Nguyen Son <valueislife@gmail.com>
2020-05-04 16:38:55 +0700

    + download file - chưa test =))

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -53,7 +53,7 @@ router.post('/tutor-list', checkAuth, async (req, res) => {
         })
     }
 
-    if (checkUser.level !== 1 || checkUser.level !== 0) {
+    if (checkUser.level !== 1 && checkUser.level !== 0) {
         return res.json({
             message: 'User is not one of staff/admin'
         })
@@ -97,7 +97,7 @@ router.post('/student-list', checkAuth, async (req, res) => {
         })
     }
 
-    if (checkUser.level !== 1 || checkUser.level !== 0) {
+    if (checkUser.level !== 1 && checkUser.level !== 0) {
         return res.json({
             message: 'User is not one of staff or admin'
         })
